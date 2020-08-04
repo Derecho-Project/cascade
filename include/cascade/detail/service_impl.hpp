cdb50edbd89c3aee29b942c63d7c68175ce17c48
Weijia <songweijia@gmail.com>
2020-08-04 16:52:10 +0000

    minor bugfix for get_by_time()
    Thanks to Lichen for finding this bug.

--- include/cascade/detail/service_impl.hpp
+++ include/cascade/detail/service_impl.hpp
@@ -284,7 +284,7 @@ derecho::rpc::QueryResults<const typename SubgroupType::ValType> ServiceClient<C
         uint32_t shard_index) {
     auto& caller = external_group.template get_subgroup_caller<SubgroupType>();
     node_id_t node_id = pick_member_by_policy<SubgroupType>(subgroup_index,shard_index);
-    return caller.template p2p_send<RPC_NAME(get)>(node_id,key,ts_us);
+    return caller.template p2p_send<RPC_NAME(get_by_time)>(node_id,key,ts_us);
 }
 
 }
