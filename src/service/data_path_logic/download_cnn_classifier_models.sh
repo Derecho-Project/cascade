697bb20e224c1403993eb14229cbb5ff11f883ed
Panlichen <myles.l.pan@gmail.com>
2020-12-29 05:12:55 -0700

    add --no-check-certificate in download_cnn_model.sh

--- src/service/data_path_logic/download_cnn_classifier_models.sh
+++ src/service/data_path_logic/download_cnn_classifier_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for name in flower pet
 do
-    wget -c https://derecho.cs.cornell.edu/files/${name}-model.tar.bz2
+    wget -c https://derecho.cs.cornell.edu/files/${name}-model.tar.bz2 --no-check-certificate
     tar -jxf ${name}-model.tar.bz2
     rm -f ${name}-model.tar.bz2
 done
