fadb936167463d742ca2b568a10fcbcef7b4b64e
root <Weijia Song>
2021-05-10 13:27:20 -0400

    adapted to the latest Derecho code commit-358b52191d75b5b84f73228af67c92d20b9694a9

--- include/cascade/detail/service_impl.hpp
+++ include/cascade/detail/service_impl.hpp
@@ -78,7 +78,7 @@ Service<CascadeTypes...>::Service(const json& layout,
     new_dsms.emplace_back(context.get());
     // STEP 3 - create derecho group
     group = std::make_unique<derecho::Group<CascadeMetadataService<CascadeTypes...>,CascadeTypes...>>(
-                CallbackSet{},
+                UserMessageCallbacks{},
                 si,
                 new_dsms,
                 std::vector<derecho::view_upcall_t>{},
