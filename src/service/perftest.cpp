74438facb0a66b81b071c077b9d75f3dc0a15b33
Weijia Song <songweijia@gmail.com>
2021-07-11 15:41:05 -0400

    +separate thread for query results.

--- src/service/perftest.cpp
+++ src/service/perftest.cpp
@@ -93,9 +93,58 @@ PerfTestServer::PerfTestServer(ServiceClientAPI& capi, uint16_t port):
         }
         free(buf);
         // STEP 3 - start experiment and log
-        std::vector<std::tuple<uint64_t,uint64_t,uint64_t>> timestamp_log; // version,send_timestamp_ns,reply_timestamp_ns
-        uint32_t window_size = derecho::getConfUInt32(CONF_DERECHO_P2P_WINDOW_SIZE);
+        // synchronization data structures
+        // 1 - version,send_timestamp_ns,reply_timestamp_ns
+        std::vector<std::tuple<uint64_t,uint64_t,uint64_t>> timestamp_log;
+        // 2 - sending window and future queue
+        uint32_t                window_size = derecho::getConfUInt32(CONF_DERECHO_P2P_WINDOW_SIZE);
+        uint32_t                window_slots = window_size;
+        std::mutex              window_slots_mutex;
+        std::condition_variable window_slots_cv;
         std::queue<std::pair<uint64_t,derecho::QueryResults<std::tuple<persistent::version_t,uint64_t>>>> futures;
+        std::mutex                                                                                        futures_mutex;
+        std::condition_variable                                                                           futures_cv;
+        std::condition_variable                                                                           window_cv;
+        // 3 - all sent flag
+        std::atomic<bool>                                   all_sent(false);
+        // 4 - query thread
+        std::thread                                         query_thread(
+            [&timestamp_log,&window_slots,&window_slots_mutex,&window_slots_cv,&futures,&futures_mutex,&futures_cv,&all_sent](){
+                std::unique_lock<std::mutex> futures_lck{futures_mutex};
+                while(!all_sent || (futures.size()>0)) {
+                    // pick pending futures
+                    using namespace std::chrono_literals;
+                    while(!futures_cv.wait_for(futures_lck,500ms,[&futures,&all_sent]{return (futures.size() > 0) || all_sent;}));
+                    std::decay_t<decltype(futures)> pending_futures;
+                    futures.swap(pending_futures);
+                    futures_lck.unlock();
+                    //+---------------------------------------+
+                    //|             QUEUE UNLOCKED            |
+                    // waiting for futures with lock released.
+                    while (pending_futures.size() > 0) {
+                        auto& replies = pending_futures.front().second.get();
+                        for (auto& reply: replies) {
+                            auto version = std::get<0>(reply.second.get());
+                            uint64_t reply_timestamp_ns = get_walltime();
+                            uint64_t send_timestamp_ns = futures.front().first;
+                            timestamp_log.emplace_back(version, send_timestamp_ns, reply_timestamp_ns);
+                            break;
+                        }
+                        pending_futures.pop();
+                        {
+                            std::lock_guard<std::mutex> window_slots_lock{window_slots_mutex};
+                            window_slots ++;
+                        }
+                        window_slots_cv.notify_one();
+                    }
+                    //|            QUEUE UNLOCKED             |
+                    //+---------------------------------------+
+                    // Acquire LOCK
+                    futures_lck.lock();
+                }
+            }
+        );
+
         //TODO: control read_write_ratio
         uint64_t interval_ns = static_cast<uint64_t>(1e9/max_operation_per_second);
         uint64_t next_ns = get_walltime();
@@ -103,61 +152,33 @@ PerfTestServer::PerfTestServer(ServiceClientAPI& capi, uint16_t port):
         while(true) {
             uint64_t now_ns = get_walltime();
             if (now_ns > end_ns) {
+                all_sent.store(true);
                 break;
             }
             if (now_ns + 1000 > next_ns) {
                 usleep((now_ns - next_ns + 1000)/1000); // sleep in microseconds.
             }
-            if (futures.size() >= window_size) {
-                auto& replies = futures.front().second.get();
-                for (auto& reply: replies) {
-                    auto version = std::get<0>(reply.second.get());
-                    uint64_t reply_timestamp_ns = get_walltime();
-                    uint64_t send_timestamp_ns = futures.front().first;
-                    timestamp_log.emplace_back(version, send_timestamp_ns, reply_timestamp_ns);
-                    break;
-                }
-                futures.pop();
+            {
+                std::unique_lock<std::mutex> window_slots_lock{window_slots_mutex};
+                window_slots_cv.wait(window_slots_lock,[&window_slots]{return (window_slots > 0);});
+                window_slots --;
             }
-            while (futures.size() < window_size && ((get_walltime() + 1000) > next_ns)) {
-                next_ns += interval_ns;
-                std::function<void(QueryResults<std::tuple<persistent::version_t,uint64_t>>)> future_appender = 
-                    [&futures](QueryResults<std::tuple<persistent::version_t,uint64_t>>&& query_results){
-                        futures.emplace(get_walltime(),std::move(query_results));
-                    };
-                on_subgroup_type_index_with_return_no_trigger(
-                    std::decay_t<decltype(capi)>::subgroup_type_order.at(object_pool.subgroup_type_index),
-                    future_appender,
-                    this->capi.template put, objects.at(get_walltime()%NUMBER_OF_DISTINCT_OBJECTS));
-            }
-            // extract ready futures.
-            while (futures.size() > 0) {
-                if (!futures.front().second.is_ready()) {
-                    break;
-                }
-                auto& replies = futures.front().second.get();
-                for (auto& reply: replies) {
-                    auto version = std::get<0>(reply.second.get());
-                    uint64_t reply_timestamp_ns = get_walltime();
-                    uint64_t send_timestamp_ns = futures.front().first;
-                    timestamp_log.emplace_back(version, send_timestamp_ns, reply_timestamp_ns);
-                    break;
-                }
-                futures.pop();
-            }
+            next_ns += interval_ns;
+            std::function<void(QueryResults<std::tuple<persistent::version_t,uint64_t>>)> future_appender = 
+                [&futures,&futures_mutex,&futures_cv](QueryResults<std::tuple<persistent::version_t,uint64_t>>&& query_results){
+                    uint64_t timestamp_ns = get_walltime();
+                    std::unique_lock<std::mutex> lock{futures_mutex};
+                    futures.emplace(timestamp_ns,std::move(query_results));
+                    lock.unlock();
+                    futures_cv.notify_one();
+                };
+            on_subgroup_type_index_with_return_no_trigger(
+                std::decay_t<decltype(capi)>::subgroup_type_order.at(object_pool.subgroup_type_index),
+                future_appender,
+                this->capi.template put, objects.at(get_walltime()%NUMBER_OF_DISTINCT_OBJECTS));
         }
         // wait for all pending futures.
-        while (futures.size() > 0) {
-            auto& replies = futures.front().second.get();
-            for (auto& reply: replies) {
-                auto version = std::get<0>(reply.second.get());
-                uint64_t reply_timestamp_ns = get_walltime();
-                uint64_t send_timestamp_ns = futures.front().first;
-                timestamp_log.emplace_back(version, send_timestamp_ns, reply_timestamp_ns);
-                break;
-            }
-            futures.pop();
-        }
+        query_thread.join();
         // write output to file
         std::ofstream outfile(output_filename);
         outfile << "#version,send_ts_us,acked_ts_us" << std::endl;
