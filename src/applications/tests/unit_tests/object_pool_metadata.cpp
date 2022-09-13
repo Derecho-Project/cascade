50fe373a0fd483d0bd71a7e61d6bba9de552e774
Weijia Song <songweijia@gmail.com>
2022-09-13 02:53:15 -0400

    a series of bugfixes.
    1) create_null_object_cb() for DefaultObjectPoolMetadata is now in service_types.hpp; 2) some fixes go with json 3.11.1

--- src/applications/tests/unit_tests/object_pool_metadata.cpp
+++ src/applications/tests/unit_tests/object_pool_metadata.cpp
@@ -2,20 +2,18 @@
 
 using namespace derecho::cascade;
 
-using opm_t = ObjectPoolMetadata<VolatileCascadeStoreWithStringKey,PersistentCascadeStoreWithStringKey,TriggerCascadeNoStoreWithStringKey>;
-
 int main(int argc, char** argv) {
     uint8_t buf[4096];
-    opm_t opm;
+    DefaultObjectPoolMetadataType opm;
     opm.to_bytes(buf);
     opm.subgroup_type_index = 1;
     opm.deleted = true;
     std::cout << opm << std::endl;
-    std::cout << *(mutils::from_bytes<opm_t>(nullptr,buf)) << std::endl;
+    std::cout << *(mutils::from_bytes<DefaultObjectPoolMetadataType>(nullptr,buf)) << std::endl;
 
-    std::cout << "VolatileCascadeStoreWithStringKey index is " << opm_t::get_subgroup_type_index<VolatileCascadeStoreWithStringKey>() << std::endl;
-    std::cout << "PersistentCascadeStoreWithStringKey index is " << opm_t::get_subgroup_type_index<PersistentCascadeStoreWithStringKey>() << std::endl;
-    std::cout << "TriggerCascadeNoStoreWithStringKey index is " << opm_t::get_subgroup_type_index<TriggerCascadeNoStoreWithStringKey>() << std::endl;
-    std::cout << "int index is " << opm_t::get_subgroup_type_index<int>() << std::endl;
+    std::cout << "VolatileCascadeStoreWithStringKey index is " << DefaultObjectPoolMetadataType::get_subgroup_type_index<VolatileCascadeStoreWithStringKey>() << std::endl;
+    std::cout << "PersistentCascadeStoreWithStringKey index is " << DefaultObjectPoolMetadataType::get_subgroup_type_index<PersistentCascadeStoreWithStringKey>() << std::endl;
+    std::cout << "TriggerCascadeNoStoreWithStringKey index is " << DefaultObjectPoolMetadataType::get_subgroup_type_index<TriggerCascadeNoStoreWithStringKey>() << std::endl;
+    std::cout << "int index is " << DefaultObjectPoolMetadataType::get_subgroup_type_index<int>() << std::endl;
     return 0;
 }
