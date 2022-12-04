d3a1ac42c53f9bc8ed5d5f660a2d3c1c1b7261c5
Michael Ye <mly32@cornell.edu>
2022-12-04 18:37:11 -0500

    style: format readme and tests

--- src/service/fuse/testfuse.py
+++ src/service/fuse/testfuse.py
@@ -1,176 +1,279 @@
 #!/usr/bin/env python3
 
-from derecho.cascade.client import ServiceClientAPI
-import subprocess
-import multiprocessing
-import threading
-import os
-import sys,getopt
-import stat
-import shutil
-import filecmp
-import tempfile
-import time
 import errno
-import sys
+import filecmp
+import getopt
+import multiprocessing
+import os
 import queue
-from tempfile import NamedTemporaryFile
+import shutil
+import stat
+import subprocess
+import sys
+import tempfile
+import threading
+import time
 from contextlib import contextmanager
-from util import (wait_for_mount, compare_dirs, umount, cleanup, base_cmdline,
-                  safe_sleep, basename,  test_printcap,
-                  fuse_proto, powerset)
 from os.path import join as pjoin
+from tempfile import NamedTemporaryFile
+
+from derecho.cascade.client import ServiceClientAPI
+from util import (
+    base_cmdline,
+    basename,
+    cleanup,
+    compare_dirs,
+    fuse_proto,
+    powerset,
+    safe_sleep,
+    test_printcap,
+    umount,
+    wait_for_mount,
+)
 
 TEST_FILE = __file__
 
-with open(TEST_FILE, 'rb') as fh:
+with open(TEST_FILE, "rb") as fh:
     TEST_DATA = fh.read()
+
 
 def name_generator(__ctr=[0]):
     __ctr[0] += 1
-    return 'testfile_%d' % __ctr[0]
+    return "testfile_%d" % __ctr[0]
+
 
 options = []
-if sys.platform == 'linux':
-    options.append('clone_fd')
+if sys.platform == "linux":
+    options.append("clone_fd")
 
 
 def invoke_directly(mnt_dir, name, options):
-    cmdline = base_cmdline + [ pjoin(basename, 'example', name),
-                               '-f', mnt_dir, '-o', ','.join(options) ]
-    if name == 'cascade_fuse_client':
+    cmdline = base_cmdline + [
+        pjoin(basename, "example", name),
+        "-f",
+        mnt_dir,
+        "-o",
+        ",".join(options),
+    ]
+    if name == "cascade_fuse_client":
         # supports single-threading only
-        cmdline.append('-s')
+        cmdline.append("-s")
     return cmdline
 
 
 def readdir_inode(dir):
-    cmd = base_cmdline + [ pjoin(basename, 'test', 'readdir_inode'), dir ]
-    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                          universal_newlines=True) as proc:
+    cmd = base_cmdline + [pjoin(basename, "test", "readdir_inode"), dir]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True) as proc:
         lines = proc.communicate()[0].splitlines()
     lines.sort()
     return lines
 
 
-def error_msg( run_id, testcase, output, expected):
+def error_msg(run_id, testcase, output, expected):
     msg = "[" + run_id + "]" + testcase + "(output--expected):"
-    msg += "(" + ','.join(output) 
-    msg += " -- " + ','.join(expected) + ")"
+    msg += "(" + ",".join(output)
+    msg += " -- " + ",".join(expected) + ")"
     return msg
 
-    
-put_keys = ['key0', '/a/a1/key0', '/b/b1/key1', '/c/c1/c2/key2']
-put_values = ['00',  '00000000', '11111111', '22222222']
 
-vcss_expected_keys = [".cascade","key-key0", "key-\\a\\a1\\key0"]
-pcss_expected_keys = [".cascade","key-\\b\\b1\\key1","key-\\c\\c1\\c2\\key2"]
+put_keys = ["key0", "/a/a1/key0", "/b/b1/key1", "/c/c1/c2/key2"]
+put_values = ["00", "00000000", "11111111", "22222222"]
+
+vcss_expected_keys = [".cascade", "key-key0", "key-\\a\\a1\\key0"]
+pcss_expected_keys = [".cascade", "key-\\b\\b1\\key1", "key-\\c\\c1\\c2\\key2"]
 
 objp_expected_second_level = [".cascade", "a", "b", "c"]
-objp_expected_third_level = [[], [".cascade", "a1"], [".cascade", "b1"], [".cascade", "c1"]]
-objp_expected_keys = [[], [".cascade", "key0"], [".cascade", "key1"], [".cascade", "c2"]]
+objp_expected_third_level = [
+    [],
+    [".cascade", "a1"],
+    [".cascade", "b1"],
+    [".cascade", "c1"],
+]
+objp_expected_keys = [
+    [],
+    [".cascade", "key0"],
+    [".cascade", "key1"],
+    [".cascade", "c2"],
+]
 objp_expected_keys2 = [".cascade", "key2"]
-
 
 
 def initial_setup():
     capi = ServiceClientAPI()
     print("----------- CASCADE INITIAL SETUP   -----------")
-    capi.put(put_keys[0], bytes(put_values[0],'utf-8'), subgroup_type='VolatileCascadeStoreWithStringKey', subgroup_index=0, shard_index=0)
-    capi.create_object_pool('/a/a1', 'VolatileCascadeStoreWithStringKey', 0)
-    capi.create_object_pool("/b/b1",'PersistentCascadeStoreWithStringKey', 0)
-    capi.create_object_pool("/c/c1/c2",'PersistentCascadeStoreWithStringKey', 0)
+    capi.put(
+        put_keys[0],
+        bytes(put_values[0], "utf-8"),
+        subgroup_type="VolatileCascadeStoreWithStringKey",
+        subgroup_index=0,
+        shard_index=0,
+    )
+    capi.create_object_pool("/a/a1", "VolatileCascadeStoreWithStringKey", 0)
+    capi.create_object_pool("/b/b1", "PersistentCascadeStoreWithStringKey", 0)
+    capi.create_object_pool("/c/c1/c2", "PersistentCascadeStoreWithStringKey", 0)
     time.sleep(5)
-    
-    capi.put(put_keys[1], bytes(put_values[1],'utf-8'),blocking=True)
-    capi.put(put_keys[2],bytes(put_values[2],'utf-8'),blocking=True)
-    res = capi.put(put_keys[3], bytes(put_values[3],'utf-8'),blocking=True)
+
+    capi.put(put_keys[1], bytes(put_values[1], "utf-8"), blocking=True)
+    capi.put(put_keys[2], bytes(put_values[2], "utf-8"), blocking=True)
+    res = capi.put(put_keys[3], bytes(put_values[3], "utf-8"), blocking=True)
     if res:
         odict = res.get_result()
         # print("------- THRID put result: " + f"{str(odict)}")
 
-    
+
 # Test 1. check the directory for Cascade subgroups
 def tst_sg_dirs(mnt_dir, run_id=""):
     # First-level(subgroup_type) check
     mnt_first_level = os.listdir(mnt_dir)
-    expected_first_level =  ['.cascade', 'MetadataService', 'PersistentCascadeStoreWithStringKey', 'VolatileCascadeStoreWithStringKey', 'TriggerCascadeNoStoreWithStringKey','ObjectPools' ]
-    error_m = error_msg(run_id, "first level", mnt_first_level, expected_first_level) 
+    expected_first_level = [
+        ".cascade",
+        "MetadataService",
+        "PersistentCascadeStoreWithStringKey",
+        "VolatileCascadeStoreWithStringKey",
+        "TriggerCascadeNoStoreWithStringKey",
+        "ObjectPools",
+    ]
+    error_m = error_msg(run_id, "first level", mnt_first_level, expected_first_level)
     assert compare_dirs(mnt_first_level, expected_first_level), error_m
     sg_expected_second_level = ["subgroup-0"]
     sg_expected_third_level = ["shard-0"]
     # Second-level(subgroup_index) check
     for subgroup_type in expected_first_level:
-        if(subgroup_type=='.cascade' or subgroup_type=='MetadataService' or subgroup_type=='ObjectPools'):
+        if (
+            subgroup_type == ".cascade"
+            or subgroup_type == "MetadataService"
+            or subgroup_type == "ObjectPools"
+        ):
             continue
         mnt_second_level = os.listdir(mnt_dir + "/" + subgroup_type)
         assert compare_dirs(mnt_second_level, sg_expected_second_level)
         # Third-level(shard) check
-        mnt_third_level = os.listdir(mnt_dir + "/" + subgroup_type + "/" + sg_expected_second_level[0])
+        mnt_third_level = os.listdir(
+            mnt_dir + "/" + subgroup_type + "/" + sg_expected_second_level[0]
+        )
         assert compare_dirs(mnt_third_level, sg_expected_third_level)
         # Forth-level(key) check
-        if(subgroup_type == 'PersistentCascadeStoreWithStringKey'):
-            mnt_forth_level = os.listdir(mnt_dir + "/" + subgroup_type + "/" + sg_expected_second_level[0] + "/" + sg_expected_third_level[0])
-            error_m = error_msg(run_id, "PCSS forthlevel", mnt_forth_level, pcss_expected_keys)
+        if subgroup_type == "PersistentCascadeStoreWithStringKey":
+            mnt_forth_level = os.listdir(
+                mnt_dir
+                + "/"
+                + subgroup_type
+                + "/"
+                + sg_expected_second_level[0]
+                + "/"
+                + sg_expected_third_level[0]
+            )
+            error_m = error_msg(
+                run_id, "PCSS forthlevel", mnt_forth_level, pcss_expected_keys
+            )
             assert compare_dirs(mnt_forth_level, pcss_expected_keys), error_m
-        if(subgroup_type == 'VolatileCascadeStoreWithStringKey'):
-            mnt_forth_level = os.listdir(mnt_dir + "/" + subgroup_type + "/" + sg_expected_second_level[0] + "/" + sg_expected_third_level[0])
-            assert compare_dirs(mnt_forth_level, vcss_expected_keys) 
-    print("["+ run_id +"]"+ "--- Test1 SubgroupTypes directories pass! ---")
+        if subgroup_type == "VolatileCascadeStoreWithStringKey":
+            mnt_forth_level = os.listdir(
+                mnt_dir
+                + "/"
+                + subgroup_type
+                + "/"
+                + sg_expected_second_level[0]
+                + "/"
+                + sg_expected_third_level[0]
+            )
+            assert compare_dirs(mnt_forth_level, vcss_expected_keys)
+    print("[" + run_id + "]" + "--- Test1 SubgroupTypes directories pass! ---")
 
-    
- # Test 2. check the directory for Cascade object_pools
+
+# Test 2. check the directory for Cascade object_pools
 def tst_objp_dirs(mnt_dir, run_id=""):
     mnt_objp_root = os.listdir(mnt_dir + "/" + "ObjectPools")
-    error_m = error_msg(run_id, "object pool test", mnt_objp_root, objp_expected_second_level)
+    error_m = error_msg(
+        run_id, "object pool test", mnt_objp_root, objp_expected_second_level
+    )
     assert compare_dirs(mnt_objp_root, objp_expected_second_level), error_m
-    for i in range(1,4):
-        mnt_objp_dir = os.listdir(mnt_dir + "/" + "ObjectPools" + "/" + objp_expected_second_level[i])
-        error_m = error_msg(run_id, f"object pool test{str(i)}", mnt_objp_dir, objp_expected_third_level[i])
+    for i in range(1, 4):
+        mnt_objp_dir = os.listdir(
+            mnt_dir + "/" + "ObjectPools" + "/" + objp_expected_second_level[i]
+        )
+        error_m = error_msg(
+            run_id,
+            f"object pool test{str(i)}",
+            mnt_objp_dir,
+            objp_expected_third_level[i],
+        )
         assert compare_dirs(mnt_objp_dir, objp_expected_third_level[i])
-        mnt_objp_key = os.listdir(mnt_dir + "/" + "ObjectPools" + "/" + objp_expected_second_level[i] + "/" + objp_expected_third_level[i][1])
+        mnt_objp_key = os.listdir(
+            mnt_dir
+            + "/"
+            + "ObjectPools"
+            + "/"
+            + objp_expected_second_level[i]
+            + "/"
+            + objp_expected_third_level[i][1]
+        )
         assert compare_dirs(mnt_objp_key, objp_expected_keys[i])
-        if(i == 3):
-            mnt_objp_subdir = os.listdir(mnt_dir + "/" + "ObjectPools" + "/" + objp_expected_second_level[i] + "/" + objp_expected_third_level[i][1] + "/" + objp_expected_keys[i][1])
+        if i == 3:
+            mnt_objp_subdir = os.listdir(
+                mnt_dir
+                + "/"
+                + "ObjectPools"
+                + "/"
+                + objp_expected_second_level[i]
+                + "/"
+                + objp_expected_third_level[i][1]
+                + "/"
+                + objp_expected_keys[i][1]
+            )
             assert compare_dirs(mnt_objp_subdir, objp_expected_keys2)
     print("[" + run_id + "]" + "--- Test2 ObjectPools directories pass! ---")
-                    
+
 
 # Test 3. check the open and read content of files
 def tst_content(mnt_dir, run_id=""):
     # 1. subgroup content test
     subgroup_type = "/VolatileCascadeStoreWithStringKey/"
-    for i in range(1,2):
-        sg_mnt_key = mnt_dir + subgroup_type + "subgroup-0/shard-0/" +vcss_expected_keys[i+1]
-        with open(sg_mnt_key, 'r') as fh:
+    for i in range(1, 2):
+        sg_mnt_key = (
+            mnt_dir + subgroup_type + "subgroup-0/shard-0/" + vcss_expected_keys[i + 1]
+        )
+        with open(sg_mnt_key, "r") as fh:
             output = fh.read()
-            error_m = error_msg(run_id, f"sg content{sg_mnt_key}", output, put_values[i])
+            error_m = error_msg(
+                run_id, f"sg content{sg_mnt_key}", output, put_values[i]
+            )
             assert output == put_values[i], error_m
     subgroup_type = "/PersistentCascadeStoreWithStringKey/"
-    for i in range(2,4):
-        sg_mnt_key = mnt_dir + subgroup_type + "subgroup-0/shard-0/" +pcss_expected_keys[i-1]
-        with open(sg_mnt_key, 'r') as fh:
+    for i in range(2, 4):
+        sg_mnt_key = (
+            mnt_dir + subgroup_type + "subgroup-0/shard-0/" + pcss_expected_keys[i - 1]
+        )
+        with open(sg_mnt_key, "r") as fh:
             output = fh.read()
-            error_m = error_msg(run_id, f"sg content{sg_mnt_key}", output, put_values[i])
+            error_m = error_msg(
+                run_id, f"sg content{sg_mnt_key}", output, put_values[i]
+            )
             assert output == put_values[i], error_m
     # 2. Objectpool content test
     object_pool_dir = "/ObjectPools"
-    for i in range(1,4):
+    for i in range(1, 4):
         objp_mnt_key = mnt_dir + object_pool_dir + put_keys[i]
-        with open(objp_mnt_key, 'r') as fh:
+        with open(objp_mnt_key, "r") as fh:
             output = fh.read()
-            error_m = error_msg(run_id, f"objp content{objp_mnt_key}", output, put_values[i])
+            error_m = error_msg(
+                run_id, f"objp content{objp_mnt_key}", output, put_values[i]
+            )
             assert output == put_values[i], error_m
-    print("["+ run_id +"]" + "--- Test3 read content pass! ---")
+    print("[" + run_id + "]" + "--- Test3 read content pass! ---")
+
 
 # def tst_attr(mnt_dir):
-    
+
+
 def cascade_fuse_mount(mnt_dir):
     # 1. create mounting piont
     subprocess.Popen(["mkdir", mnt_dir])
-    cmd_input = base_cmdline + [ pjoin(basename, "cascade_fuse_client"), mnt_dir, "-f"]
+    cmd_input = base_cmdline + [pjoin(basename, "cascade_fuse_client"), mnt_dir, "-f"]
     # 2. run cascade_fuse_client
-    mount_process = subprocess.Popen(cmd_input, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    mount_process = subprocess.Popen(
+        cmd_input, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
     # try:
     wait_for_mount(mount_process, mnt_dir)
     # except:
@@ -179,18 +282,19 @@ def cascade_fuse_mount(mnt_dir):
     # else:
     #     umount(mnt_dir)
 
+
 def cascade_fuse_umount(mnt_dir):
     # 1. umount the mounting process at mnt_dir
     umount(mnt_dir)
     # 2. delete the mounted directory
-    subprocess.Popen(["rm", "-r",mnt_dir])
+    subprocess.Popen(["rm", "-r", mnt_dir])
     print(" ~~~~~~~~~~~~~  FINISHED cascade_fuse_client unmount  ~~~~~~~~~~~\n")
 
 
 def fuse_test_cases(mnt_dir, run_id):
-    tst_sg_dirs(mnt_dir,run_id)
-    tst_objp_dirs(mnt_dir,run_id)
-    tst_content(mnt_dir,run_id)
+    tst_sg_dirs(mnt_dir, run_id)
+    tst_objp_dirs(mnt_dir, run_id)
+    tst_content(mnt_dir, run_id)
     # tst_attr(mnt)
 
 
@@ -199,33 +303,41 @@ def main(argv):
     process_num = 1
     thread_num = 1
     try:
-        opts, args = getopt.getopt(argv,"ht:p:",["threadN=","processN="])
+        opts, args = getopt.getopt(argv, "ht:p:", ["threadN=", "processN="])
     except getopt.GetoptError:
-        print('fusetest.py  -t <threadNumber> -p <processNumber>')
+        print("fusetest.py  -t <threadNumber> -p <processNumber>")
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print ('fusetest.py  -t <threadNumber> -p <processNumber>')
+        if opt == "-h":
+            print("fusetest.py  -t <threadNumber> -p <processNumber>")
             sys.exit()
         elif opt in ("-t", "--ifile"):
             thread_num = int(arg)
         elif opt in ("-p", "--ofile"):
             process_num = int(arg)
-    
+
     cascade_process = multiprocessing.Process(target=initial_setup)
     cascade_process.start()
     cascade_process.join()
 
-    mnt_dir = pjoin(basename,"testdir")  # use build/src/service/fuse/testdir as mounting point
+    mnt_dir = pjoin(
+        basename, "testdir"
+    )  # use build/src/service/fuse/testdir as mounting point
     fuse_process = multiprocessing.Process(target=cascade_fuse_mount, args=(mnt_dir,))
     fuse_process.start()
-    fuse_process.join()   # used when fuse not run in foreground in fuse_client.cpp
-    
+    fuse_process.join()  # used when fuse not run in foreground in fuse_client.cpp
+
     # multi-processing test cases
     test_processes = []
     for i in range(process_num):
         run_id = "process_" + str(i)
-        proc = multiprocessing.Process(target=fuse_test_cases, args=(mnt_dir,run_id,))
+        proc = multiprocessing.Process(
+            target=fuse_test_cases,
+            args=(
+                mnt_dir,
+                run_id,
+            ),
+        )
         test_processes.append(proc)
         proc.start()
     for proc in test_processes:
@@ -235,7 +347,13 @@ def main(argv):
     test_threads = []
     for i in range(thread_num):
         run_id = "thread_" + str(i)
-        thread = threading.Thread(target=fuse_test_cases, args=(mnt_dir,run_id,))
+        thread = threading.Thread(
+            target=fuse_test_cases,
+            args=(
+                mnt_dir,
+                run_id,
+            ),
+        )
         test_threads.append(thread)
         thread.start()
     for thread in test_threads:
@@ -244,5 +362,6 @@ def main(argv):
     # fuse_process.terminate()
     cascade_fuse_umount(mnt_dir)
 
+
 if __name__ == "__main__":
-    main(sys.argv[1:]) 
+    main(sys.argv[1:])
--- src/service/fuse/util.py
+++ src/service/fuse/util.py
@@ -1,48 +1,47 @@
 #!/usr/bin/env python3
-import subprocess
-import pytest
+import itertools
 import os
+import re
 import stat
+import subprocess
+import sys
 import time
 from os.path import join as pjoin
-import sys
-import re
-import itertools
+
+import pytest
 
 basename = os.path.dirname(os.path.abspath(__file__))
 fusermount3_dir = "/home/yy354/opt-dev/bin"
 
 
 def test_printcap():
-    cmdline = base_cmdline + [ pjoin(basename, 'example', 'printcap') ]
-    proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE,
-                            universal_newlines=True)
+    cmdline = base_cmdline + [pjoin(basename, "example", "printcap")]
+    proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE, universal_newlines=True)
     (stdout, _) = proc.communicate(30)
     assert proc.returncode == 0
 
     proto = None
     caps = set()
-    for line in stdout.split('\n'):
-        if line.startswith('\t'):
+    for line in stdout.split("\n"):
+        if line.startswith("\t"):
             caps.add(line.strip())
             continue
 
-        hit = re.match(r'Protocol version: (\d+)\.(\d+)$', line) 
+        hit = re.match(r"Protocol version: (\d+)\.(\d+)$", line)
         if hit:
             proto = (int(hit.group(1)), int(hit.group(2)))
 
     return (proto, caps)
 
 
-def wait_for_mount(mount_process, mnt_dir,
-                   test_fn=os.path.ismount):
+def wait_for_mount(mount_process, mnt_dir, test_fn=os.path.ismount):
     elapsed = 0
     while elapsed < 20:
         if test_fn(mnt_dir):
             print(" -------- Fuse mount succeed -------")
             return True
         if mount_process.poll() is not None:
-            pytest.fail('file system process terminated prematurely')
+            pytest.fail("file system process terminated prematurely")
         time.sleep(0.1)
         elapsed += 0.1
     pytest.fail("mountpoint failed to come up")
@@ -56,28 +55,27 @@ def compare_dirs(output, expected):
         if not dir in output:
             return False
     return True
-    
+
 
 def cleanup(mount_process, mnt_dir):
     # Don't bother trying Valgrind if things already went wrong
 
-    if 'bsd' in sys.platform or 'dragonfly' in sys.platform:
-        cmd = [ 'umount', '-f', mnt_dir ]
+    if "bsd" in sys.platform or "dragonfly" in sys.platform:
+        cmd = ["umount", "-f", mnt_dir]
     else:
-        cmd = [pjoin(basename, 'util', 'fusermount3'),
-                         '-z', '-u', mnt_dir]
-    subprocess.call(cmd, stdout=subprocess.DEVNULL,
-                    stderr=subprocess.STDOUT)
+        cmd = [pjoin(basename, "util", "fusermount3"), "-z", "-u", mnt_dir]
+    subprocess.call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     mount_process.terminate()
     try:
         mount_process.wait(1)
     except subprocess.TimeoutExpired:
         mount_process.kill()
 
+
 def umount(mnt_dir):
 
-    if 'bsd' in sys.platform or 'dragonfly' in sys.platform:
-        cmdline = [ 'umount', mnt_dir ]
+    if "bsd" in sys.platform or "dragonfly" in sys.platform:
+        cmdline = ["umount", mnt_dir]
     else:
         # fusermount3 will be setuid root, so we can only trace it with
         # valgrind if we're root
@@ -85,21 +83,17 @@ def umount(mnt_dir):
             cmdline = base_cmdline
         else:
             cmdline = []
-        cmdline = cmdline + [ pjoin(fusermount3_dir, 'fusermount3'),
-                              '-z', '-u', mnt_dir ]
+        cmdline = cmdline + [pjoin(fusermount3_dir, "fusermount3"), "-z", "-u", mnt_dir]
     subprocess.check_call(cmdline)
     assert not os.path.ismount(mnt_dir)
 
- 
-
-
 
 def safe_sleep(secs):
-    '''Like time.sleep(), but sleep for at least *secs*
+    """Like time.sleep(), but sleep for at least *secs*
     `time.sleep` may sleep less than the given period if a signal is
     received. This function ensures that we sleep for at least the
     desired time.
-    '''
+    """
 
     now = time.time()
     end = now + secs
@@ -109,26 +103,30 @@ def safe_sleep(secs):
 
 
 def powerset(iterable):
-  s = list(iterable)
-  return itertools.chain.from_iterable(
-      itertools.combinations(s, r) for r in range(len(s)+1))
+    s = list(iterable)
+    return itertools.chain.from_iterable(
+        itertools.combinations(s, r) for r in range(len(s) + 1)
+    )
 
 
 # Use valgrind if requested
-if os.environ.get('TEST_WITH_VALGRIND', 'no').lower().strip() \
-   not in ('no', 'false', '0'):
-    base_cmdline = [ 'valgrind', '-q', '--' ]
+if os.environ.get("TEST_WITH_VALGRIND", "no").lower().strip() not in (
+    "no",
+    "false",
+    "0",
+):
+    base_cmdline = ["valgrind", "-q", "--"]
 else:
     base_cmdline = []
 
 # Try to use local fusermount3
-os.environ['PATH'] = '%s:%s' % (pjoin(basename, 'util'), os.environ['PATH'])
+os.environ["PATH"] = "%s:%s" % (pjoin(basename, "util"), os.environ["PATH"])
 # Put example binaries on PATH
-os.environ['PATH'] = '%s:%s' % (pjoin(basename, 'example'), os.environ['PATH'])
+os.environ["PATH"] = "%s:%s" % (pjoin(basename, "example"), os.environ["PATH"])
 
 try:
     (fuse_proto, fuse_caps) = test_printcap()
 except:
     # Rely on test to raise error
-    fuse_proto = (0,0)
+    fuse_proto = (0, 0)
     fuse_caps = set()
