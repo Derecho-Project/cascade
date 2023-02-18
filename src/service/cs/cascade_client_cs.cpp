a641dc842ab463104779703df164d7f5b5e28561
Peter <peterlingwu@gmail.com>
2023-02-18 14:30:04 -0500

    Revise CMakeLists to not pollute source dir with C# build files

--- src/service/cs/cascade_client_cs.cpp
+++ src/service/cs/cascade_client_cs.cpp
@@ -3,26 +3,17 @@
 #endif//not __EXTERNAL_CLIENT__
 
 #include <cascade/cascade.hpp>
+#include <cascade/service_types.hpp>
 #include <cascade/service_client_api.hpp>
 #include <derecho/core/detail/rpc_utils.hpp>
 #include <derecho/persistent/PersistentInterface.hpp>
 #include <string>
-#include <vector>
 
 // ----------------
 // Regular C++ code
 // ----------------
 
 using namespace derecho::cascade;
-
-/**
-    For error messages.
-*/
-extern "C" void print_red(const char* msg) {
-    std::cout << "\033[1;31m"
-              << msg
-              << "\033[0m" << std::endl;
-}
 
 extern "C" ServiceClientAPI& get_service_client_ref() {
     return ServiceClientAPI::get_service_client();
