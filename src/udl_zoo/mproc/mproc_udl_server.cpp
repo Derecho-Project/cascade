9f89ff4207b06c31eb5b3fb152a73c75d82079b2
Weijia Song <songweijia@gmail.com>
2023-11-09 22:14:16 -0500

    [WIP] mproc server
    working on mproc server.

--- src/udl_zoo/mproc/mproc_udl_server.cpp
+++ src/udl_zoo/mproc/mproc_udl_server.cpp
@@ -1,3 +1,28 @@
 #include "mproc_udl_server.hpp"
 
-//TODO:
+namespace derecho {
+namespace cascade {
+
+MProcUDLServer::MProcUDLServer(const struct mproc_udl_server_arg_t& arg):
+    stop_flag(false) {
+    //TODO
+    // 1 - load udl to this->ocdpo
+    // 2 - attach to three ringbuffers
+    // 3 - initialize mproc_ctxt
+    // 4 - start the upcall thread pool
+}
+
+void MProcUDLServer::start(bool wait) {
+    //TODO
+}
+
+MProcUDLServer::~MProcUDLServer() {
+    //TODO
+    // 1 - stop
+    // 2 - destroy mproc_ctxt
+    // 3 - detach ring buffers
+    // 4 - unload ocdpo
+}
+
+}
+}
